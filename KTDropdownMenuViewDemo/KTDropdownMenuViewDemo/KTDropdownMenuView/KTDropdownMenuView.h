edd39755d4961a06b9ebb6c916b621c18d5bdf74
Kevin Tu <819931323@qq.com>
2015-10-12 23:09:37 +0800

    添加文件

---
+++ KTDropdownMenuViewDemo/KTDropdownMenuViewDemo/KTDropdownMenuView/KTDropdownMenuView.h
@@ -0,0 +1,13 @@
+//
+//  KTDropdownMenuView.h
+//  KTDropdownMenuViewDemo
+//
+//  Created by tujinqiu on 15/10/12.
+//  Copyright © 2015年 tujinqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KTDropdownMenuView : UIView
+
+@end
---
+++ KTDropdownMenuViewDemo/KTDropdownMenuViewDemo/KTDropdownMenuView/KTDropdownMenuView.m
@@ -0,0 +1,21 @@
+//
+//  KTDropdownMenuView.m
+//  KTDropdownMenuViewDemo
+//
+//  Created by tujinqiu on 15/10/12.
+//  Copyright © 2015年 tujinqiu. All rights reserved.
+//
+
+#import "KTDropdownMenuView.h"
+
+@implementation KTDropdownMenuView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
